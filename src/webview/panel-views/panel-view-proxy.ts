5679de63fcdc2935a119d82a2ebd0bc7629218ba
Gino Cardillo <gino-c@gmx.ch>
2021-10-17 17:14:40 +0200

    VOOD-46: Add visjs example.
    + Create proxy for potential future views

---
+++ src/webview/panel-views/panel-view-proxy.ts
@@ -0,0 +1,10 @@
+
+export interface PanelViewProxy {
+  getHtml: () => string;
+  updatePanel: () => UpdatePanelViewCommand;
+}
+
+export interface UpdatePanelViewCommand {
+  [key: string]: any;
+  command: string;
+}
