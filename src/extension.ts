dcb90f55f4de15e3aac5487322eff8a58f2aa37a
paschuer <pascal.schuermann@ost.ch>
2021-10-20 10:13:32 +0200

    VOOD-24: Implementation DebugSessionManager

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import { commands, ExtensionContext } from 'vscode';
 import { DebuggerPanel } from './webview/debugger-panel';
 import { VisjsPanelView } from './webview/panel-views/visjs-panel-view';
+import { DebugPrinter } from './debug-adapter/DebugPrinter';
+import { DebugEventManager } from './debug-adapter/DebugEventManager';
 
 export function activate(context: ExtensionContext): void {
   new Extension(context);
@@ -16,6 +18,11 @@ class Extension {
   constructor(context: ExtensionContext) {
     this.debuggerPanel = new DebuggerPanel(context, new VisjsPanelView(context));
 
+    const dp = new DebugPrinter 
+    const dsm = new DebugEventManager(dp);
+
     context.subscriptions.push(commands.registerCommand('visual-oo-debugger.openDebugger', () => this.debuggerPanel.openPanel()));
+
+
   }
 }
