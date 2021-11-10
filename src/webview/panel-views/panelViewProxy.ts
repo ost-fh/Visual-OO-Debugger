ce788a59d47d549a8a9337ad59c4f88464587d84
GinoCardillo-OST <gino.cardillo@ost.ch>
2021-11-10 14:52:43 +0100

    VOOD-50: Add a button to load the previous state.

--- src/webview/panel-views/panelViewProxy.ts
+++ src/webview/panel-views/panelViewProxy.ts
@@ -1,12 +1,14 @@
+import { Webview } from 'vscode';
 import { PanelViewInput } from '../../model/panelViewInput';
 
 export interface PanelViewProxy {
-  getHtml: () => string;
+  getHtml: (webview: Webview) => string;
   updatePanel: (panelViewInput: PanelViewInput) => PanelViewCommand;
   exportPanel: () => PanelViewCommand;
   startRecordingPanel: () => PanelViewCommand;
   stopRecordingPanel: () => PanelViewCommand;
   teardownPanelView: () => void;
+  stepBack?: () => PanelViewCommand;
 }
 
 export interface PanelViewCommand {
