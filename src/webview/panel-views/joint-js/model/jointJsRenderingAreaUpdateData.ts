957e2f528df53461ff27c8da936cdd11c26180b5
Alexandre Lagadec (OST) <alexandre.lagadec@ost.ch>
2022-06-17 16:32:17 +0200

    fix: simplify the update model radically
    VOOD-173

--- src/webview/panel-views/joint-js/model/jointJsRenderingAreaUpdateData.ts
+++ src/webview/panel-views/joint-js/model/jointJsRenderingAreaUpdateData.ts
@@ -5,23 +5,29 @@ import { Structure } from '../../../../object-diagram/model/structure';
 export type StructureToBeAdded = Structure;
 export type StructureToBeUpdated = Pick<Structure, 'id' | 'value'>;
 export type StructureToBeRemoved = Pick<Structure, 'id'>;
+export type StructureToBeRestored = StructureToBeAdded;
 
 export type FieldToBeAdded = Field;
 export type FieldToBeUpdated = Pick<Field, 'parentId' | 'name' | 'value'>;
 export type FieldToBeRemoved = Pick<Field, 'parentId' | 'name'>;
+export type FieldToBeRestored = FieldToBeAdded;
 
 export type ReferenceToBeAdded = Reference;
 export type ReferenceToBeUpdated = Reference;
 export type ReferenceToBeRemoved = Reference;
+export type ReferenceToBeRestored = ReferenceToBeAdded;
 
 export interface JointJsRenderingAreaUpdateData {
   structuresToBeAdded: StructureToBeAdded[];
   structuresToBeUpdated: StructureToBeUpdated[];
   structuresToBeRemoved: StructureToBeRemoved[];
+  structuresToBeRestored: StructureToBeRestored[];
   fieldsToBeAdded: FieldToBeAdded[];
   fieldsToBeUpdated: FieldToBeUpdated[];
   fieldsToBeRemoved: FieldToBeRemoved[];
+  fieldsToBeRestored: FieldToBeRestored[];
   referencesToBeAdded: ReferenceToBeAdded[];
   referencesToBeUpdated: ReferenceToBeUpdated[];
   referencesToBeRemoved: ReferenceToBeRemoved[];
+  referencesToBeRestored: ReferenceToBeRestored[];
 }
